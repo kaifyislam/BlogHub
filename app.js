40e95e4306aafb4321902310e79fe923d1adaab2
kaifyislam <91680041+kaifyislam@users.noreply.github.com>
2023-10-13 19:43:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,31 +72,31 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.CLIENT_ID,
-//   clientSecret: process.env.CLIENT_SECRET,
-//   callbackURL: "http://localhost:3000/auth/google/secrets",
-//   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-// },
-// function(accessToken, refreshToken, profile, cb) {
-//   console.log(profile);
-//   User.findOrCreate({ username: profile.displayName, googleId: profile.id }, function(err, user) {
-//     return cb(err, user)
-//   });
-// }
-// ));
-// passport.use(new FacebookStrategy({
-//   clientID: process.env.APP_ID,
-//   clientSecret: process.env.APP_SECRET,
-//   callbackURL: "http://localhost:3000/auth/facebook/secrets",
-// },
-// function(accessToken, refreshToken, profile, cb) {
-//   console.log(profile);
-//   User.findOrCreate({ username: profile.displayName, facebookId: profile.id }, function(err, user) {
-//     return cb(err, user);
-//   });
-// }
-// ));
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "https://kaifys-hub.onrender.com/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({ username: profile.displayName, googleId: profile.id }, function(err, user) {
+    return cb(err, user)
+  });
+}
+));
+passport.use(new FacebookStrategy({
+  clientID: process.env.APP_ID,
+  clientSecret: process.env.APP_SECRET,
+  callbackURL: "https://kaifys-hub.onrender.com/auth/google/secrets",
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({ username: profile.displayName, facebookId: profile.id }, function(err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 // Middleware to check if the user is authenticated
 function ensureAuthenticated(req, res, next) {
