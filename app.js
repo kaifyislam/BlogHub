f019340696018ebea87262f58321300dcb66b7e3
kaifyislam <91680041+kaifyislam@users.noreply.github.com>
2023-10-14 20:05:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-  clientID: '67213339349-ub7ci9ssl1r4shjtpgovon6avq7nn9qi.apps.googleusercontent.com',
-  clientSecret: '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',
+  clientID: '67213339349-9ohbrfep3oi6hvo4gf7u8n055ncdrciv.apps.googleusercontent.com',
+  clientSecret: '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',
   callbackURL: "https://kaifys-blog.onrender.com/auth/google/secrets",
   
 },
